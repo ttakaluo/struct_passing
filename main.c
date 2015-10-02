c26fa05232db4b83988d7caa80113df3ca287cf8
Teemu Takaluoma <teemu.takaluoma@gmail.com>
2015-10-02 18:30:14 +0300

    read arguments

--- main.c
+++ main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "print_out.h"
 
 int main(int argc, char *argv[]) {
 
+
+	if ( argc != 3 ) {
+		printf ("usage: %s iterations num1\n", argv[0] );
+		exit(-1);
+	}
+
 	struct arg_struct args;
 
-	args.arg1 = 10;
-	args.arg2 = 20;
+	args.arg1 = atoi(argv[1]);
+	args.arg2 = atoi(argv[2]);
 
 	print_out((void *) &args);
 
