d871c272e59eee89d3d408f1f9f4efd7b9e93510
Teemu Takaluoma <teemu.takaluoma@gmail.com>
2015-10-02 18:18:25 +0300

    Remove unnecessary define.

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include "print_out.h"
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-
-      struct arg_struct {
-      	int arg1;
-      	int arg2;
-      };
 	struct arg_struct args;
 
-      args.arg1 = 10;
-      args.arg2 = 20;
+	args.arg1 = 10;
+	args.arg2 = 20;
 
 	print_out((void *) &args);
-	
+
 	return 0;
 }
